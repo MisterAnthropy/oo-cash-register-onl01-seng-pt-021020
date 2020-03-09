42960b8f6034ca8d55a6973a737867d20c7e8e5a
Tyler Croley <tjcroley@outlook.com>
2020-03-09 20:59:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,14 @@
 #binding.pry 
 class CashRegister
   
-  attr_accessor :total, :discount 
+  attr_accessor :total, :discount, :price, :total
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @total = []
   end
+  
+  def add_item()
   
 end
