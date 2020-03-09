8903a6911637bab29879a312f701eb746befa334
Tyler Croley <tjcroley@outlook.com>
2020-03-09 20:51:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   
   attr_accessor :total, :employee_discount
   
-  def initialize(employee_discount = nil)
+  def initialize(employee_discount)
     @total = 0.00
     @employee_discount = 20
   end
